85236ac9ba80021f0e68e109cdffb30e7ecaa386
Colleen <colleenhiggins87@gmail.com>
2019-12-03 11:25:13 -0800

    add and edit note, delete note, auth redirects

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_mTDjb81w361titAXGngxhXWB00nPbfe0nk",
   s3: {
     REGION: "us-west-1",
     BUCKET: "colleen-note-app-uploads"
@@ -11,6 +13,6 @@ export default {
     REGION: "us-east-2",
     USER_POOL_ID: "us-east-2_R33mZD5S2",
     APP_CLIENT_ID: "7v0qoqedd3be3ak394a71qgre0",
-    IDENTITY_POOL_ID: " us-east-2:5c41cbe6-8f53-4430-9622-e2d7c7ad030b"
+    IDENTITY_POOL_ID: "us-east-2:59f7cd80-01fe-4da1-ac45-f5d2f89d66c0"
   }
 }
